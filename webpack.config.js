54adfc91ef17d459abec3b22fca99bbccd9c2e96
shun-okamoto <okamons8811@gmail.com>
2023-02-11 23:38:55 +0900

    設定のミスを修正

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,23 @@
 const path = require('path')
 module.exports = {
   // bundleの起点ファイル
-  entry: {
-    bundle: './src/index.ts'
-  },
+  entry: './src/index.ts',
   // bundleの出力ファイル
   output: {
     // __dirname = 現在のパス
     // path.join(a, b) = aのパスのb
     path: path.join(__dirname, 'dist'),
     // [name].js = bundle.js(entryのプロパティ)
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   // importする時に、拡張子を描かなくて良くする為の設定
   resolve: {
-    extensions: ['ts', '.js']
+    extensions: ['.ts', '.js']
   },
-  // localサーバーが参照するJS
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    // サーバー立ち上がり時自動でブラウザを立ち上げる
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     open: true
   },
   // loaderの設定を行うことが多い
