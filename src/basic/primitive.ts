d43dbdc4da30abde82dd3f44653f3194740e55d4
shun-okamoto <okamons8811@gmail.com>
2023-02-12 14:16:25 +0900

    プリミティブ型の学習

---
+++ src/basic/primitive.ts
@@ -0,0 +1,13 @@
+export default function primitiveSample() {
+  const name = 'Okamon'
+  console.log(typeof name, name)
+
+  const age = 26
+  console.log(typeof age, age)
+
+  const isSingle = false
+  console.log(typeof isSingle, isSingle)
+
+  const isOver20 = age >= 20
+  console.log(typeof isOver20, isOver20)
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import World from './world'
 const root: HTMLElement | null = document.getElementById('root')
 const world = new World('Hello Shun!!!')
 world.sayHello(root)
+
+// 基本の型定義
+import { primitiveSample } from './basic/index'
+
+primitiveSample()
